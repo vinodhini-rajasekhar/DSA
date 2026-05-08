3a24471a6f0f69dfbfb89ae095273cfb5c973ccc
Vinodhini Rajasekhar <vinodhini98r@gmail.com>
2026-05-07 20:12:47 -0400

    Time: 44 ms (65.01%) | Memory: 19.2 MB (88.27%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ""
+        max_length = max(len(word1),len(word2))
+        for i in range(max_length):
+            if i < len(word1):
+                res += word1[i]
+            if i < len(word2):
+                res += word2[i]
+            
+        return res
+                
+
+        
